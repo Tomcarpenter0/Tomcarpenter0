aff4433bb0fc53e2b9247892ec9f01e9ef74594b
faprikaa <muammarm28@gmail.com>
2026-07-02 09:31:51 +0700

    try fix cs jss again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-    // GitHub Pages user site — replace with your own https://<username>.github.io
     site: "https://tomcarpenter0.github.io",
+    base: "/Tomcarpenter0/",
 });
