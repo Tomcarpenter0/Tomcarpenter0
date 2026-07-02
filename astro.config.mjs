18512c2014675c0df66583d34aee209c2d9629e1
faprikaa <muammarm28@gmail.com>
2026-07-02 09:28:24 +0700

    try fix cs js in cicd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  // GitHub Pages user site — replace with your own https://<username>.github.io
-  site: 'https://tomcarpen.github.io',
+    // GitHub Pages user site — replace with your own https://<username>.github.io
+    site: "https://tomcarpenter0.github.io",
 });
